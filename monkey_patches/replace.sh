d7d040720eaa5ee96f59a392d9eabee489c86bbc
Ingus Pretkalnins <ingus.pretkalnins@tilde.lv>
2026-02-10 12:40:21 +0000

    Add script that distributes monkey patches correctly in nvcr.io/nvidia/nemo:25.11 docker container

---
+++ monkey_patches/replace.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Script automagically replaces code installed in the 25.11 NeMo docker container.
+
+# Modifications to Megatron-LM
+cp ./gpt_dataset.py /opt/megatron-lm/megatron/core/datasets/gpt_dataset.py
+
+# Modifications to NeMo
+cp ./llama.py /opt/NeMo/nemo/collections/llm/gpt/model/llama.py
+
+# Modifying tensorRT
+cp ./megatron.py /opt/TensorRT-Model-Optimizer/modelopt/torch/nas/plugins/megatron.py
+
+# Modifying tensor_engine The
+#   The flash attention override.
+cp ./dot_product_attention.py /opt/venv/lib/python3.12/site-packages/transformer_engine/pytorch/attention/dot_product_attention/dot_product_attention.py
+
+
+# Debug prints
+#cp ./attention.py /opt/megatron-lm/megatron/core/transformer/attention.py
+
